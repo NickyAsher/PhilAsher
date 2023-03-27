22f5ed490d0e5b63a5e08f63b71bf6a4d2b4affe
NickyAsher <nick-asher@hotmail.com>
2023-03-28 00:14:17 +0100

    HTML changes

--- philasher/src/App.js
+++ philasher/src/App.js
@@ -8,11 +8,11 @@ import Stack from 'react-bootstrap/Stack';
 
 function TheNavBar(){
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar bg="dark" variant="dark" fixed="top">
       <Container>
         <Stack gap={0}>
           <Navbar.Brand href="#home">Phil Asher Building and Joinery</Navbar.Brand>
-          <Nav className="me-auto">
+          <Nav className="m-auto">
             <Nav.Link href="#about">About</Nav.Link>
             <Nav.Link href="#projects">Projects</Nav.Link>
             <Nav.Link href="#contact">Contact</Nav.Link>
