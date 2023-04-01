74611d3a6a2956134dfd6fcad6cf95e6b0ff9847
NickyAsher <nick-asher@hotmail.com>
2023-04-01 14:04:34 +0100

    Add about me and project

--- philasher/src/App.js
+++ philasher/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Stack from 'react-bootstrap/Stack';
+import Carousel from 'react-bootstrap/Carousel';
 
 
 function TheNavBar(){
@@ -23,20 +25,67 @@ function TheNavBar(){
   )
 }
 
+function ProjectCarousel(){
+  return (
+    <Carousel bg="dark" variant="dark">
+      <Carousel.Item>
+        <img
+            className="ProjectImage"
+            src="images\IMG-20220227-WA0019.jpg"
+            alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>Pizza Oven</h3>
+          <p>A custom-designed redbrick Pizza Oven, with accompanying outdoor kitchen/bar utilities</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+            className="ProjectImage"
+            src="images\IMG-20220227-WA0007.jpg"
+            alt="Second slide"
+        />
+        <Carousel.Caption>
+          <h3>Bespoke walk-in wardrobe</h3>
+          <p>Handmade walk-in wardrobe designed to maximize storage while maintaining the looks and feel of the property</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  )
+}
+
 
 function App() {
   return (
     <div className="App">
       <TheNavBar/>
-      <section className='ParallaxImage Image1'>
-      </section>
+      <section className='ParallaxImage Image1'></section>
       <section>
-        <div className='MainContent'>
-         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <div className='ParallaxHeader'>
+          <p><b>Bespoke building work & Joinery</b></p>
         </div>
       </section>
-      <section className='ParallaxImage Image2'>
+      <section className='ParallaxImage Image2'></section>
+      <section>
+        <div className='ParallaxHeader'>
+          <p><b>Kitchens</b></p>
+        </div>
       </section>
+      <section className='ParallaxImage Image3'></section>
+      <section>
+        <div className='ParallaxHeader'>
+          <p><b>Driveways</b></p>
+        </div>
+      </section>
+      <section className='ParallaxImage Image4'></section>
+      <section>
+        <div className='MainContent' id='about'>
+          <h2><b>About</b></h2>
+          <p>A self-employed builder with decades of experience.<br/>Services ranging from simple repairs, to kitchen fittings, to even bespoke building projects.<br/>Based in Lymm, operating within the Cheshire - South Manchester Area</p>
+          <br/>
+        </div>
+      </section>
+      <ProjectCarousel/>
     </div>
   );
 }
