a123535ceda77a4f38db170512b3e77bbf97e93a
NickyAsher <nick-asher@hotmail.com>
2023-04-01 01:01:06 +0100

    Added parallax effect

--- philasher/src/App.js
+++ philasher/src/App.js
@@ -18,7 +18,6 @@ function TheNavBar(){
             <Nav.Link href="#contact">Contact</Nav.Link>
           </Nav>
         </Stack>
-        
       </Container>  
     </Navbar>
   )
@@ -29,6 +28,15 @@ function App() {
   return (
     <div className="App">
       <TheNavBar/>
+      <section className='ParallaxImage Image1'>
+      </section>
+      <section>
+        <div className='MainContent'>
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </section>
+      <section className='ParallaxImage Image2'>
+      </section>
     </div>
   );
 }
